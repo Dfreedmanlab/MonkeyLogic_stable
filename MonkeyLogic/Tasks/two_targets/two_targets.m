9057550f683165c5669a72b6a478d0eeed035c5c
Edward Ryklin <ryklin@gmail.com>
2015-10-28 15:29:17 -0400

    Switched from acquirefix to acquiretargets Added showcursor('on') Added comments

--- MonkeyLogic/Tasks/two_targets/two_targets.m
+++ MonkeyLogic/Tasks/two_targets/two_targets.m
@@ -1,11 +1,29 @@
+% This is an example MonkeyLogic task that demonstrates very basic use of
+% an analog joystick to select one of two targets
+%
+% Two targest will appear (one above and one below the center of the
+% screen). Moving the joystick to either target will cause the trial to
+% end.
+% 
+% ShowCursor is set to on, and a small gray filled disc will appear on the
+% stimulus display, which represents the location of the joystick.
+%
+% Make sure the joystick is mapped in the GUI to analog inputs, or else
+% this task will cause an error to appear. If you wish to use an eye
+% tracker instead of a joystick you need to change the 'aqcuiretarget'
+% command to 'acquirefix'.
+%
+% Oct 28, 2015   Last Modified by Edward Ryklin(edward@ryklinsoftware.com)
+
+
 left = 1;           %left target number
 right = 2;          %right target number
 windowSize = 1.5;   %degrees of visual angle surrounding the target. I believe this is the diameter (not radius)
 duration = 3000;    %in milliseconds
 
 toggleobject([left right], 'Status', 'on');
-
-ontarget = eyejoytrack('acquirefix', [left right], windowSize, duration);
+showcursor('on');
+ontarget = eyejoytrack('acquiretarget', [left right], windowSize, duration);
 if ~ontarget,
      trialerror(4); % no response
      toggleobject([left right], 'Status', 'off');
@@ -13,5 +31,6 @@ if ~ontarget,
      return
 end
 
+showcursor('off');
 
 set_iti(0);
