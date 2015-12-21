82636995a95b596000220d03c912fd1e3d709c9f
Edward Ryklin <ryklin@gmail.com>
2015-12-21 13:02:23 -0500

    Small patch to fix error reported in griddedInterpolant when samples is less than 2

--- MonkeyLogic/touchscreen_dataclean.m
+++ MonkeyLogic/touchscreen_dataclean.m
@@ -3,6 +3,7 @@ function [cleaned_data] = touchscreen_dataclean(data_stream)
 %data_stream = [-180 -180 -180 -180 -180 -180 -180 1 2 3 -180 -180 1 2 3 -180 2 -180 -180 -180 -180 -180 -180]; %test case
 % -180 is the BLANK value to replace with a valid sample
 
-cleaned_data = interp1(find(data_stream~=-180),data_stream(data_stream~=-180), 1:length(data_stream),'nearest','extrap');
-
+if (length(data_stream) > 2)
+    cleaned_data = interp1(find(data_stream~=-180),data_stream(data_stream~=-180), 1:length(data_stream),'nearest','extrap');
+end
 % end of function
