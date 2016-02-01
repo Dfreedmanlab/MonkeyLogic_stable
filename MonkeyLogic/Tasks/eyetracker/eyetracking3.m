b98026deed51ca5670b39db7292017da8fd4ffd6
Edward Ryklin <ryklin@gmail.com>
2016-02-01 10:36:40 -0500

    Fixed eyetracking and touchscreen tasks which track for hold/release

--- MonkeyLogic/Tasks/eyetracker/eyetracking3.m
+++ MonkeyLogic/Tasks/eyetracker/eyetracking3.m
@@ -5,11 +5,11 @@
 % shaped stimuli will cause them to be selected (switch from not filled to
 % filled colors). 
 % 
-% Nov 19, 2015   Last Modified by Edward Ryklin(edward@ryklinsoftware.com)
+% February 01, 2016   Last Modified by Edward Ryklin(edward@ryklinsoftware.com)
 
 windowSize = 1.5;   % in degrees of visual angle (DVA) I believe this is the diameter (not radius)
 fixDuration = 2000; % duration in milliseconds to test for a fixation
-holdDuration = 500;
+holdDuration = 5000;
 
 gazeTargetLeftNotFilled    = 1;
 gazeTargetLeftFilled       = 2;
@@ -21,10 +21,11 @@ ontargetRightTarget = 0;
 
 showcursor('on');
 scene_timer = tic;
-target_fixed = [0, 0];
 
 toggleobject(gazeTargetLeftNotFilled, 'Status', 'on');
 toggleobject(gazeTargetRightNotFilled, 'Status', 'on');
+toggleobject(gazeTargetLeftFilled, 'Status', 'off');
+toggleobject(gazeTargetRightFilled, 'Status', 'off');
 
 ontargets = eyejoytrack('acquirefix', [gazeTargetLeftNotFilled gazeTargetRightNotFilled],  windowSize, fixDuration);     % it does not matter if you track the filled or not filled target since they overlap eachother in space
 
@@ -32,14 +33,13 @@ if (ontargets == 1)
 
 	toggleobject(gazeTargetLeftNotFilled, 'Status', 'off');
     toggleobject(gazeTargetLeftFilled, 'Status', 'on');
-    target_fixed(1) = 1;
-    disp('<<< MonkeyLogic >>> Target 1 Fixed');
+    disp('<<< eyetracking3.m >>> Object 1 acquirefix');
 
     ontargets = eyejoytrack('holdfix', gazeTargetLeftFilled,  windowSize, holdDuration);     % it does not matter if you track the filled or not filled target since they overlap eachother in space
 
     if (ontargets == 1)
     	trialerror(1);
-        disp('<<< MonkeyLogic >>> Target 1 held');
+        disp('<<< eyetracking3.m >>> Object 1 holdfix');
 	end
         
 end
@@ -48,16 +48,15 @@ if (ontargets == 2)
         
 	toggleobject(gazeTargetRightNotFilled, 'Status', 'off');
     toggleobject(gazeTargetRightFilled, 'Status', 'on');
-    target_fixed(2) = 1;
-    disp('<<< MonkeyLogic >>> Target 2 Fixed');
+    disp('<<< eyetracking3.m >>> Object 2 acquirefix');
 
     ontargets = eyejoytrack('holdfix', gazeTargetRightFilled,  windowSize, holdDuration);     % it does not matter if you track the filled or not filled target since they overlap eachother in space
 
     if (ontargets == 1)
     	trialerror(2);
-        disp('<<< MonkeyLogic >>> Target 2 held');
+        disp('<<< eyetracking3.m >>> Object 2 holdfix');
 	end
         
 end
 
-set_iti(750);
+set_iti(750); % in milliseconds
--- MonkeyLogic/Tasks/touchscreen/touch_release.m
+++ MonkeyLogic/Tasks/touchscreen/touch_release.m
@@ -2,27 +2,28 @@
 % touchscreen to select stimulus.
 %
 % Moving the mouse/touchscreen controlled cursor over one of two square
-% shaped stimuli will cause them to be selected (switch from not filled to
-% filled colors). Unlike the other tracking sample, this one will terminate
-% the trial only after both stimuli are selected.
+% shaped stimuli, then clicking and releasing,
+% will cause that stimuli to be selected (switch from not filled to
+% filled colors). 
 % 
-% The position of the cursor is displayed using showcursor('on')
-% This task is much more efficient than the other sample. However, it can
-% not display any advanced touch location stimuli.
+% The position of the cursor is displayed using showcursor('on'). 
 %
-% Jan 27, 2016   Last Modified by Edward Ryklin(edward@ryklinsoftware.com)
+% February 01, 2016   Last Modified by Edward Ryklin(edward@ryklinsoftware.com)
 %
 % NOTE : Please make sure that the Enable Mouse/System Keys option located 
 % in the Advanced system menu is set to ON
 
 windowSize = 1.5;   % in degrees of visual angle (DVA) I believe this is the diameter (not radius)
-fixDuration = 5000; % duration in milliseconds to test for a touch/fixation
+fixDuration = 2000; % duration in milliseconds to test for a fixation
+holdDuration = 5000;
    
 touchTargetLeftNotFilled    = 1;
 touchTargetLeftFilled       = 2;
 touchTargetRightNotFilled   = 3;
 touchTargetRightFilled      = 4;
 
+ontargetLeftTarget = 0;
+ontargetRightTarget = 0;
 numdev = xgldevices;
 for devicenum = 1:numdev,
 	xglshowcursor(devicenum, 0);
@@ -33,30 +34,41 @@ scene_timer = tic;
 
 toggleobject(touchTargetLeftNotFilled, 'Status', 'on');
 toggleobject(touchTargetRightNotFilled, 'Status', 'on');
+toggleobject(touchTargetLeftFilled, 'Status', 'off');
+toggleobject(touchTargetRightFilled, 'Status', 'off');
 
-while toc(scene_timer) < 10
+ontargets = eyejoytrack('touchtarget', [touchTargetLeftNotFilled touchTargetRightNotFilled], windowSize, fixDuration);     % it does not matter if you track the filled or not filled target since they overlap eachother in space
 
-	ontargets = eyejoytrack('releasetarget', [touchTargetLeftNotFilled touchTargetRightNotFilled],  windowSize, fixDuration);     % it does not matter if you track the filled or not filled target since they overlap eachother in space
+if (ontargets == 1)
+
+	toggleobject(touchTargetLeftNotFilled, 'Status', 'off');
+    toggleobject(touchTargetLeftFilled, 'Status', 'on');
+    disp('<<< touch_release.m >>> Object 1 touchtarget');
+
+    ontargets = eyejoytrack('releasetarget', touchTargetLeftFilled,  windowSize, holdDuration);     % it does not matter if you track the filled or not filled target since they overlap eachother in space
 
     if (ontargets == 1)
-        toggleobject(touchTargetLeftNotFilled, 'Status', 'off');
-        toggleobject(touchTargetLeftFilled, 'Status', 'on');
-        trialerror(0);
-        disp('<<< MonkeyLogic >>> Target 1 Selected');
-        break;
-    end
-
-    if (ontargets == 2)
-        toggleobject(touchTargetRightNotFilled, 'Status', 'off');
-        toggleobject(touchTargetRightFilled, 'Status', 'on');
-        trialerror(1);
-        disp('<<< MonkeyLogic >>> Target 2 Selected');
-        break;
-    end
-
-    idle(20);  % if this idle command is missing there will be a buffer overrun error when the trial completes
-    
+    	trialerror(1);
+        disp('<<< touch_release.m >>> Object 1 releasetarget');
+	end
+        
 end
+
+if (ontargets == 2)
+        
+	toggleobject(touchTargetRightNotFilled, 'Status', 'off');
+    toggleobject(touchTargetRightFilled, 'Status', 'on');
+    disp('<<< touch_release.m >>> Object 2 touchtarget');
+
+    ontargets = eyejoytrack('releasetarget', touchTargetRightFilled,  windowSize, holdDuration);     % it does not matter if you track the filled or not filled target since they overlap eachother in space
+
+    if (ontargets == 1)
+    	trialerror(2);
+        disp('<<< touch_release.m >>> Object 2 releasetarget');
+	end
+        
+end
+
 
 numdev = xgldevices;
 for devicenum = 1:numdev,
